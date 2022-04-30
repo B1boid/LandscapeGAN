29d6d05d477c682e842cc351691d47a0e02a529b
B1boid <amigodos@yandex.ru>
2022-04-30 11:36:55 +0300

    debug

--- scripts/obj_detection/FindObjects.py
+++ scripts/obj_detection/FindObjects.py
@@ -14,13 +14,18 @@ class InferenceConfig(config.__class__):
 class FindObjects:
   def __init__(self):
 
+    print("Log-1")
     self.config = InferenceConfig()
     DEVICE = "/gpu:0"
     WEIGHTS_PATH = "networks/obj_detection/mask_rcnn_1.h5"
-
+    print("Log-2")
     with tf.device(DEVICE):
       self.model = modellib.MaskRCNN(mode="inference",model_dir='',config=self.config)
+    print("Log-3")
     self.model.load_weights(WEIGHTS_PATH, by_name=True)
+    print("Log-4")
+    self.model.keras_model._make_predict_function()
+    print("Log-5")
 
     self.graph = tf.get_default_graph()
 
@@ -28,13 +33,18 @@ class FindObjects:
       1 : 'moon',
       2 : 'tree'
     }
+    print("Log-5-")
 
 
   def __call__(self, image):
+    print("Log-6")
     image = np.asarray(image)
-    self.model.keras_model._make_predict_function()
+    print("Log-7")
     with self.graph.as_default():
       results = self.model.detect([image], verbose=1)[0]
+    print("Log-8")
+    clear_session()
+    print("Log-9")
 
     all_classes = [self.dict2names[cur_class] for cur_class in results['class_ids']]
     if len(all_classes) > 0:
@@ -42,6 +52,6 @@ class FindObjects:
       all_masks = [mask.squeeze() for mask in masks]
     else:
       all_masks = []
-    
+    print("Log-10")
     
     return all_classes, all_masks
